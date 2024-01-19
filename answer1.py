4abe9ce6a1b765e2814d6c04d6b5582731b2d3f3
wilsonk121 <99470223+wilsonk121@users.noreply.github.com>
2024-01-19 22:17:30 +0800

    Add files via upload

--- answer1.py
+++ answer1.py
@@ -27,28 +27,6 @@ def appearance(input_string):
     print(appearance_ratio)
     return appearance_ratio
 
-ingest('oursky:uk:dev')
-ingest('oursky:hk:design')
-ingest('oursky:hk:pm')
-ingest('oursky:hk:dev')
-ingest('skymaker')
-appearance('oursky')
-# > 0.8
-appearance('oursky:hk')
-# > 0.6
-
-ingest('skymaker:london:ealing:dev')
-ingest('skymaker:london:design')
-ingest('skymaker:london:design')
-ingest('skymaker:man:pm')
-ingest('skymaker:man:pm')
-appearance('skymaker:man')
-# > 0.2
-appearance('skymaker:lon')
-# > 0
-appearance('london:skymaker')
-# > 0
-
 #Time complexity: O(n*m), where n is the length of the collection, m is the length of the longest element in the collection
 #Time complexity: O(n*m), where n is the length of the collection, m is the length of the longest element in the collection
 
